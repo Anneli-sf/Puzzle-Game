4b84fc204b09fc3c9c1b57fd0e1f3bf7c8142b3e
Anneli Savchenko <foranneli@gmail.com>
2022-10-19 12:09:48 +0200

    feat: created html, styles, started DOM

---
+++ gem-puzzle/src/index.js
@@ -0,0 +1,6 @@
+import './scss/style.scss';
+// import './js/bootstrap.bundle.min';
+import './js/main-page';
+import './js/shuffle';
+
+
---
+++ gem-puzzle/src/js/shuffle.js
@@ -0,0 +1,9 @@
+export function shuffle(arr) {
+  const shuffledArray = [...arr];
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+
+  return shuffledArray;
+}
